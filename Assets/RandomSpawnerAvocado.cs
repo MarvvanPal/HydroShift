dcbe8209a5819ad6b13d5fa4f028fc2ab0f67246
Marv van Pal <79196690+MarvvanPal@users.noreply.github.com>
2022-11-16 19:54:02 +0100

    Animation Button 2 - Spawning Cubes
    - Gamescene splitted into avocado and chocolate
    - adjusted spawning cubes on button 2 for avocado
    - second scene for chocolate still open to do
    - Size adjustment of the cubes

---
+++ Assets/RandomSpawnerAvocado.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSpawnerAvocado : MonoBehaviour
+{
+    public float update;
+    public GameObject cubePrefab;
+    public int cube_counter = 0;
+
+    void Awake() {
+        update = 0.0f;
+    }
+
+    IEnumerator Start() {
+    yield return new WaitForSeconds(0.2f);
+    }
+
+    // Update is called once per frame
+    void Update() {
+    
+        update += Time.deltaTime;
+        if (update > 0.01f){
+            if (cube_counter < 500) {  
+            Vector3 randomSpanPosition=new Vector3(Random.Range(-1,1),Random.Range(3,5), Random.Range(2,3));
+            //Vector3 randomSpanPosition=new Vector3(0, 8, 2);
+            Instantiate(cubePrefab,randomSpanPosition, Quaternion.identity);
+            }
+            update = 0.0f;
+            cube_counter = cube_counter +1;
+            }
+    }
+}
--- Assets/RandomSpawnerChocolate.cs
+++ Assets/RandomSpawnerChocolate.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RandomSpawner : MonoBehaviour
+public class RandomSpawnerChocolate : MonoBehaviour
 {
     public GameObject cubePrefab;
 
