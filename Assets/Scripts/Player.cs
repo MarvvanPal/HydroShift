be09a7aa3cf435092d0c836fda81b31b03bd26dd
schilling-p <paulschilling@posteo.de>
2023-03-07 19:50:35 +0100

    added Prefabs for spawner and cube to-be-spawned, added scripts

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Player : MonoBehaviour
+{
+    private WaterCube Cube;
+    float volume = 7000f;
+    Tuple<float, float, float> dimensionsOfCube;
+    float volInM3;
+    public Animator animController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cube = GetComponent<WaterCube>();
+        volInM3 = Cube.volInM3(volume);
+        dimensionsOfCube = Cube.GetCubeDimensions(volInM3);        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void Main()
+    {
+        Cube.SpawnCube(dimensionsOfCube.Item1, dimensionsOfCube.Item2, dimensionsOfCube.Item3);
+        Animator cubeAnim = Cube.GetComponent<Animator>();
+        cubeAnim.runtimeAnimatorController = animController.runtimeAnimatorController;
+
+    }
+}
