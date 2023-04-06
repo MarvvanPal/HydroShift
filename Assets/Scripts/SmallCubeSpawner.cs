81a10873dd6ba3aa4ef69e34957e36315ce09fdb
schilling-p <paulschilling@posteo.de>
2023-04-06 12:27:46 +0200

    instance not set error in jsonManager line 169 and 182

--- Assets/Scripts/SmallCubeSpawner.cs
+++ Assets/Scripts/SmallCubeSpawner.cs
@@ -17,15 +17,14 @@ public class SmallCubeSpawner : MonoBehaviour
     private float spawnRate = 0.1f;
 
     // Which item do you want the cubes to represent? 
-    private string itemName = "Avocado";
+    private string itemName = "Milk";
 
     private void Start()
     {
-        //volume = jsonManager.GetWaterConsumedPerPiece(itemName);
-        //int cubesToBeSpawned = (int)Mathf.Round(volume) / 10;
-        cubesToBeSpawned = 50;
+        volume = jsonManager.GetWaterConsumedPerPiece(itemName);
+        int cubesToBeSpawned = (int)Mathf.Round(volume) / 10;
+        //cubesToBeSpawned = 50;
         InvokeRepeating("SpawnCubeWrapper", 0f, spawnRate);
-
     }
 
     /*
@@ -55,12 +54,12 @@ public class SmallCubeSpawner : MonoBehaviour
     */
     private void SpawnCube(int amountOfCubes, int cubesToBeSpawned)
     {
-        if (amountOfCubes <= cubesToBeSpawned)
+        while (amountOfCubes <= cubesToBeSpawned)
         {
             Vector3 randomSpawnPosition = new Vector3(Random.Range(-2, 2), Random.Range(5, 8), Random.Range(2, 3));
             Instantiate(cubePrefab, randomSpawnPosition, Quaternion.identity);
-            amountOfCubes += 1;
         }
+        amountOfCubes += 1;
     }
 
     private void SpawnCubeWrapper()
