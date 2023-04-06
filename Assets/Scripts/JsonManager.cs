cf804a6622bc71f6e8f75f6a1d1d91782075c9b2
schilling-p <paulschilling@posteo.de>
2023-04-06 11:01:29 +0200

    updated database and added prefabs for small cube spawning

--- Assets/Scripts/JsonManager.cs
+++ Assets/Scripts/JsonManager.cs
@@ -27,27 +27,132 @@ public class JsonManager : MonoBehaviour
                 "Avocado",
                 new GroceryItem
                 {
-                    EAN = 1234567890,
+                    EAN = 2694560410,
                     Category = "Vegetables",
-                    WaterConsumedPerPiece = 800.0f
+                    WaterConsumedPerPiece = 100.0f,
+                    MassPerPieceInGram = 170.0f
+
                 }
             },
             {
                 "Chocolate",
                 new GroceryItem
                 {
-                    EAN = 9876543210,
+                    EAN = 9568921812,
                     Category = "Sweets",
-                    WaterConsumedPerPiece = 1700.0f
+                    WaterConsumedPerPiece = 1700.0f,
+                    MassPerPieceInGram = 100.0f
                 }
             },
             {
                 "Beef",
                 new GroceryItem
                 {
-                    EAN = 1234098765,
+                    EAN = 3494725170,
                     Category = "Meat",
-                    WaterConsumedPerPiece = 15000.0f
+                    WaterConsumedPerPiece = 4650.0f,
+                    MassPerPieceInGram = 300
+                }
+            },
+            {
+                "Millet",
+                new GroceryItem
+                {
+                    EAN = 8207461166,
+                    Category = "Cereal Grains",
+                    WaterConsumedPerPiece = 2500.0f,
+                    MassPerPieceInGram = 500.0f
+                }
+            },
+            {
+                "Toast",
+                new GroceryItem
+                {
+                    EAN = 8672704622,
+                    Category = "Bread",
+                    WaterConsumedPerPiece = 650.0f,
+                    MassPerPieceInGram = 500.0f
+                }
+            },
+            {
+                "Burger",
+                new GroceryItem
+                {
+                    EAN = 3587403851,
+                    Category = "Meat",
+                    WaterConsumedPerPiece = 2500.0f,
+                    MassPerPieceInGram = 150.0f
+                }
+            },
+            {
+                "Barley",
+                new GroceryItem
+                {
+                    EAN = 7533822919,
+                    Category = "Cereal Grains",
+                    WaterConsumedPerPiece = 650.0f,
+                    MassPerPieceInGram = 500.0f
+                }
+            },
+            {
+                "Sorghum",
+                new GroceryItem
+                {
+                    EAN = 4976045919,
+                    Category = "Cereal Grains",
+                    WaterConsumedPerPiece = 1400.0f,
+                    MassPerPieceInGram = 500.0f
+                }
+            },
+            {
+                "Cane Sugar",
+                new GroceryItem
+                {
+                    EAN = 3404068827,
+                    Category = "Baking Supplies",
+                    WaterConsumedPerPiece = 750.0f,
+                    MassPerPieceInGram = 500.0f
+                }
+            },
+            {
+                "Tea",
+                new GroceryItem
+                {
+                    EAN = 1369121209,
+                    Category = "Drinks",
+                    WaterConsumedPerPiece = 90.0f,
+                    MassPerPieceInGram = 750.0f,
+                }
+            },
+            {
+                "Coffee",
+                new GroceryItem
+                {
+                    EAN = 9817029772,
+                    Category = "Drinks",
+                    WaterConsumedPerPiece = 840.0f,
+                    MassPerPieceInGram = 750.0f
+
+                }
+            },
+            {
+                "Milk",
+                new GroceryItem
+                {
+                    EAN = 4507482257,
+                    Category = "Dairy Products",
+                    WaterConsumedPerPiece = 1000.0f,
+                    MassPerPieceInGram = 1000.0f
+                }
+            },
+            {
+                "Cheese",
+                new GroceryItem
+                {
+                    EAN = 4854009171,
+                    Category = "Dairy Products",
+                    WaterConsumedPerPiece = 2500.0f,
+                    MassPerPieceInGram = 500.0f
                 }
             }
         };
@@ -92,6 +197,7 @@ public class GroceryItem
     public long EAN;
     public string Category;
     public float WaterConsumedPerPiece;
+    public float MassPerPieceInGram;
 }
 
 [System.Serializable]
