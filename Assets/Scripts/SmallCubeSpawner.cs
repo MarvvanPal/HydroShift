599a4582fd9fdaa6fac377919856bb7d80120038
schilling-p <paulschilling@posteo.de>
2023-04-10 11:04:58 +0200

    current state 10.04.2023, 11:04, cubeSizeAdjustment

--- Assets/Scripts/SmallCubeSpawner.cs
+++ Assets/Scripts/SmallCubeSpawner.cs
@@ -7,8 +7,14 @@ public class SmallCubeSpawner : MonoBehaviour
 
     public JsonManager jsonManager;
     public GameObject cubePrefab;
-         
+
+    // adjust the size of the cubes if necessary
+    private Vector3 smallCubeSize;
+
+    // varibale to determine the amount of cubes to be spawned, will be calculated
     private int cubesToBeSpawned;
+
+    // volume variable to be filled by a request to the json file
     private float volume;
 
     // How often do you want the cubes to be spawned?
