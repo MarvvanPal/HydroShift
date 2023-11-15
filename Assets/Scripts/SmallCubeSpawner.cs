a11ee7c63b2dd5f2bea413f93713e1d2e8009ee1
schilling-p <paulschilling@posteo.de>
2023-11-15 10:12:27 +0100

    add new edit mode tests to smallCubeSpawner, refactor smallCubeSpawner

--- Assets/Scripts/SmallCubeSpawner.cs
+++ Assets/Scripts/SmallCubeSpawner.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
@@ -33,18 +34,22 @@ public class SmallCubeSpawner : MonoBehaviour
   
     public void SpawnSmallCubes()
     {
-        volume = 1700f/*jsonManager.GroceryItems[itemName].waterConsumedPerPiece*/;
-        cubesToBeSpawned = CalculateCubesToBeSpawned(volume, maxAmountOfCubes);
-        smallCubeSize = GetSmallCubeDimensions(cubesToBeSpawned, maxAmountOfCubes, volume);
+        volume = 2500f/*jsonManager.GroceryItems[itemName].waterConsumedPerPiece*/;
+        cubesToBeSpawned = CalculateCubesToBeSpawned(volume);
+        smallCubeSize = GetSmallCubeDimensions(volume, cubesToBeSpawned);
+        Debug.Log(smallCubeSize);
         
-        StartCoroutine(SpawningSmallCubes(cubesToBeSpawned, spawnRate, smallCubeSize));
+        StartCoroutine(SpawningSmallCubes(spawnRate));
     }
     
-    private IEnumerator SpawningSmallCubes(int cubesToBeSpawned, float delay, Vector3 smallCubeSize)
+    private IEnumerator SpawningSmallCubes(float delay)
     {
         for (int i = 1; i <= cubesToBeSpawned; i++)
         {
-            Vector3 randomSpawnPosition = new Vector3(Random.Range(-0.8f, -1.5f), Random.Range(-1, 1.5f), Random.Range(1, 2));
+            Vector3 randomSpawnPosition = new Vector3(
+                UnityEngine.Random.Range(-0.8f, -1.5f),
+                UnityEngine.Random.Range(-1, 1.5f),
+                UnityEngine.Random.Range(1, 2));
 
             GameObject spawnedCube = Instantiate(cubePrefab, randomSpawnPosition, Quaternion.identity);
             spawnedCube.transform.localScale = smallCubeSize;
@@ -54,27 +59,26 @@ public class SmallCubeSpawner : MonoBehaviour
     }   
 
 
-    internal int CalculateCubesToBeSpawned(float volume, int maxAmountOfCubes)
+    internal int CalculateCubesToBeSpawned(float volumeOfItemInLiters)
     {
-        int cubesToBeSpawned = Mathf.RoundToInt(volume / 10);
+        int amountOfCubesWith10LiterVolume = Mathf.RoundToInt(volumeOfItemInLiters / 10);
         
-        return Mathf.Clamp(cubesToBeSpawned, 0, maxAmountOfCubes);
+        return Mathf.Clamp(amountOfCubesWith10LiterVolume, 0, maxAmountOfCubes);
     }
 
 
-    internal Vector3 GetSmallCubeDimensions(int cubesToBeSpawned, int maxAmountOfCubes, float volume)
+    internal Vector3 GetSmallCubeDimensions(float volumeOfItemInLiters, int amountOfCubesToBeSpawned)
     {       
 
-        if (cubesToBeSpawned < maxAmountOfCubes)
+        if (amountOfCubesToBeSpawned < maxAmountOfCubes)
         {
             smallCubeSize = new Vector3(0.22f, 0.22f, 0.22f);
-            
         }
 
         else
         {
-            float cubeVolumeInM3 = volume / maxAmountOfCubes / 1000f;
-            float dimensions = Mathf.Pow(Mathf.Round(cubeVolumeInM3 * 100) / 100, 1f/3f);
+            float cubeVolumeInM3 = volumeOfItemInLiters / maxAmountOfCubes / 1000f;
+            float dimensions = (float)Math.Round(Mathf.Pow(cubeVolumeInM3 * 100 / 100, 1f/3f), 2);
             smallCubeSize = new Vector3(x : dimensions, y : dimensions, z : dimensions);
         }
 
--- Assets/Tests/EditMode/SmallCubeSpawnerTests.cs
+++ Assets/Tests/EditMode/SmallCubeSpawnerTests.cs
@@ -25,27 +25,33 @@ public class SmallCubeSpawnerTests
         Object.DestroyImmediate(testObject);
     }
 
-    [TestCase(50f, 30, ExpectedResult = 5)]
-    [TestCase(100f, 30, ExpectedResult = 10)]
-    [TestCase(300f, 30, ExpectedResult = 30)]
-    [TestCase(10, 30, ExpectedResult = 1)]
-    [TestCase(0, 30, ExpectedResult = 0)]
-    [TestCase(-10, 30, ExpectedResult = 0)]
-    [TestCase(10000, 30, ExpectedResult = 30)]
-    public int CalculateCubesToBeSpawned_ReturnsCorrectValue(float testVolume, int testMaxCubes)
+    [TestCase(50f, ExpectedResult = 5)]
+    [TestCase(100f, ExpectedResult = 10)]
+    [TestCase(300f,ExpectedResult = 30)]
+    [TestCase(10,ExpectedResult = 1)]
+    [TestCase(0, ExpectedResult = 0)]
+    [TestCase(-10, ExpectedResult = 0)]
+    [TestCase(10000, ExpectedResult = 30)]
+    public int CalculateCubesToBeSpawned_ReturnsCorrectValue(float testVolume)
     {
-        return spawner.CalculateCubesToBeSpawned(testVolume, testMaxCubes);
+        return spawner.CalculateCubesToBeSpawned(testVolume);
     }
     
     [TestCaseSource(nameof(GetSmallCubeDimensionsTestCases))]
-    public (float, float, float) GetSmallCubeDimensions_ReturnsCorrectDimensions(int cubesToBeSpawned, int maxAmountOfCubes, float volume)
+    public (float, float, float) GetSmallCubeDimensions_ReturnsCorrectDimensions(float volume, int testAmountOfCubesToBeSpawned)
     {
-        Vector3 result = spawner.GetSmallCubeDimensions(cubesToBeSpawned, maxAmountOfCubes, volume);
+        Vector3 result = spawner.GetSmallCubeDimensions(volume, testAmountOfCubesToBeSpawned);
         return (result.x, result.y, result.z);
     }
     
     public static IEnumerable<TestCaseData> GetSmallCubeDimensionsTestCases()
     {
-        yield return new TestCaseData(10, 30, 50f).Returns((0.22f, 0.22f, 0.22f));
+        yield return new TestCaseData(50f, 5).Returns((0.22f, 0.22f, 0.22f));
+        yield return new TestCaseData(2500f, 30).Returns((0.44f, 0.44f, 0.44f));
+        yield return new TestCaseData(15000f, 30).Returns((0.79f, 0.79f, 0.79f));
+        yield return new TestCaseData(1000f, 30).Returns((0.32f, 0.32f, 0.32f));
+        yield return new TestCaseData(150f, 30).Returns((0.17f, 0.17f, 0.17f));
+
+
     }
 }
