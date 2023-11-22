e8ebd5497bc1bed688308dcbf68edb8dbee70bc1
schilling-p <paulschilling@posteo.de>
2023-11-22 15:48:25 +0100

    refactor BigWaterCube, take functions from button to script

--- Assets/Scripts/BigWaterCube.cs
+++ Assets/Scripts/BigWaterCube.cs
@@ -8,51 +8,67 @@ using UnityEngine;
 public class BigWaterCube : MonoBehaviour
 {
     [SerializeField] private JsonManager jsonManager;
+    [SerializeField] private MeshRenderer cubeMesh;
+    [SerializeField] private MeshRenderer liquidMesh;
+    [SerializeField] private Animator animator;
 
     private string itemName = "Cheese";
     private float volume;
 
     private Tuple<float, float, float> dimensionsOfCube;
     private float volumeInCubicMeters;
-
-    void Start()
+    
+    public void AdjustCubeScaleToVolume()
     {
-        volume = jsonManager.GroceryItems[itemName].waterConsumedPerPiece;
-        volumeInCubicMeters = volInM3(volume);
+        if (volume == 0)
+        {    
+            volume = 500f;
+        }
+        
+        volumeInCubicMeters = VolInM3(volume);
         dimensionsOfCube = GetCubeDimensions(volumeInCubicMeters);
         GetComponent<Transform>().localScale = new Vector3(dimensionsOfCube.Item1, dimensionsOfCube.Item2, dimensionsOfCube.Item3);
+        
+        SetActiveRunAnimationsAndEnableMeshRenderer();
     }
 
-    private float volInM3(float volume)
+    private void SetActiveRunAnimationsAndEnableMeshRenderer()
+    {
+        gameObject.SetActive(true);
+        cubeMesh.enabled = true;
+        liquidMesh.enabled = true;
+        animator.Play("BigWaterCubeAnimation");
+    }
+
+    private static float VolInM3(float volume)
     {
         float volInM3 = volume / 1000;
         return volInM3;
     }
 
-    private Tuple<float, float, float> GetCubeDimensions(float m3)
+    private Tuple<float, float, float> GetCubeDimensions(float volumeInM3)
     {
         float width, height, length;
-
-
-        if (m3 <= 2.5)
+        
+        if (volumeInM3 <= 2.5)
         {
             width = 1;
             length = 1;
-            height = m3;
+            height = volumeInM3;
         }
 
-        else if (m3 <= 8 && m3 > 2.5)
+        else if (volumeInM3 <= 8 && volumeInM3 > 2.5)
         {
             width = 2;
             length = 2;
-            height = m3 / 4;
+            height = volumeInM3 / 4;
         }
 
         else
         {
             width = 2.5f;
             length = 2.5f;
-            height = m3 / 6.25f;
+            height = volumeInM3 / 6.25f;
         }
         return Tuple.Create(width, height, length);
     }
